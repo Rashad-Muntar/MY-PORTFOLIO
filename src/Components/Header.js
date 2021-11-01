127ab2db04c18746f12c3c46a0c1e9029aff8c55
Rashad-Muntar <muntartoure@gmail.com>
2021-11-01 15:10:32 +0000

    Add social timeline enimation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,13 +2,17 @@ import { useRef, useEffect } from 'react';
 import '../Assets/Styles/Header.css'
 import { gsap } from "gsap";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Header = () => {
     const el = useRef()
     const q = gsap.utils.selector(el)
     const tl = useRef()
+    
 
     useEffect(() => {
+    
         tl.current = gsap.timeline()
             .to(q('.my-name'),{
                 y: '0%',
@@ -20,6 +24,7 @@ const Header = () => {
                 duration: 1,
                 ease: 'sine.in'
             })
+            .to(q('.social-icon'),{opacity:1, stagger:0.3})
     }, [])
 
     return (
@@ -33,9 +38,9 @@ const Header = () => {
         </div>
 
         <div className='social'>
-            <a href='#'></a>
-            <a href='#'></a>
-            <a href='#'></a>
+            <a className='social-icon' href='#'><LinkedInIcon fontSize='large'/></a>
+            <a className='social-icon' href='#'><GitHubIcon fontSize='large'/></a>
+            <a className='social-icon' href='#'><TwitterIcon fontSize='large'/></a>
         </div>
         </section>
     )
