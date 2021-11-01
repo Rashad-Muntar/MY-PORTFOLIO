54e65b3a9cdefe97b8c187b72f76ea591df783a8
Rashad-Muntar <muntartoure@gmail.com>
2021-11-01 12:45:23 +0000

    add navbar

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import '../Assets/Styles/App.css';
+import Navbar from './Navbar';
+import Header from './Header'
+
+function App() {
+  return (
+      <main className="main-wrapper">
+        <Navbar />
+        <Header />
+      </main>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,44 @@
+import { useRef, useEffect } from 'react';
+import '../Assets/Styles/Header.css'
+import { gsap } from "gsap";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+const Header = () => {
+    const el = useRef()
+    const q = gsap.utils.selector(el)
+    const tl = useRef()
+
+    useEffect(() => {
+        tl.current = gsap.timeline()
+            .to(q('.my-name'),{
+                y: '0%',
+                duration: 2,
+                ease: 'power2.inOut',
+            })
+            .to(q('.my-job-title'),{
+                y:'0%',
+                duration: 1,
+                ease: 'sine.in'
+            })
+    }, [])
+
+    return (
+        <section className='header-wrapper' ref={el}>
+        <div className='nameWrapper'>
+            <span className='my-name'>RASHAD MUNTAR</span>
+        </div>
+
+        <div className='my-job-title-wrapper'>
+            <div className='my-job-title'>FULL-STACK DEVELOPER</div>
+        </div>
+
+        <div className='social'>
+            <a href='#'></a>
+            <a href='#'></a>
+            <a href='#'></a>
+        </div>
+        </section>
+    )
+}
+
+export default Header
