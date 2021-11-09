60512ed6a80d3083297c8571579b5b459761a660
Rashad-Muntar <muntartoure@gmail.com>
2021-11-09 18:40:43 +0000

    Add all projects over effect

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -1,31 +1,40 @@
 import '../Assets/Styles/allProjects.css';
+import checker from '../Assets/Images/checker1.png';
+import pico from '../Assets/Images/pico2.png';
+import weather from '../Assets/Images/weather2.png';
+import life from '../Assets/Images/life1.png';
+import crypto from '../Assets/Images/crypto1.png';
 
 const Allprojects = () => (
   <div className="all-project-wrapper">
     <div className="all-project-content-wtrapper">
-      <h1 className="all-pro-header-title">Projects</h1>
+      <h1 className="all-pro-header-title">Featured Projects</h1>
 
       <div className="project-cards-wrapper">
-        <div className="item item1">
-          <div className="hover-img1 imges" />
-          <h1>Pico Food</h1>
-        </div>
-        <div className="item item2">
-          <div className="hover-img2 imges" />
-          <h1>Checker</h1>
-        </div>
-        <div className="item item3">
-          <div className="hover-img3 imges" />
-          <h1>Weather</h1>
-        </div>
-        <div className="item item4">
-          <div className="hover-img4 imges" />
-          <h1>Cryplist</h1>
-        </div>
-        <div className="item item5">
-          <div className="hover-img5 imges" />
-          <h1>Life Style</h1>
-        </div>
+        <h1 className="pro-titles pico">
+          Pico Food
+          <img src={pico} alt="pico" className="img1 img" />
+        </h1>
+
+        <h1 className="pro-titles checker">
+          Checker
+          <img src={checker} alt="pico" className="img2 img" />
+        </h1>
+
+        <h1 className="pro-titles weather">
+          Weather
+          <img src={weather} alt="pico" className="img3 img" />
+        </h1>
+
+        <h1 className="pro-titles crypto">
+          Cryplist
+          <img src={crypto} alt="pico" className="img4 img" />
+        </h1>
+
+        <h1 className="pro-titles life">
+          Life Style
+          <img src={life} alt="pico" className="img5 img" />
+        </h1>
       </div>
 
     </div>
